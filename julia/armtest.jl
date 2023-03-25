3ffbcd640eb3b58a0f6239525eb75275c7b15444
matthewbbone <matthewbbone@gmail.com>
2023-03-25 10:53:59 +0000

    update

--- julia/armtest.jl
+++ julia/armtest.jl
@@ -1,4 +1,13 @@
-using PlotlyJS, Statistics, BenchmarkTools, DataFrames, JSON, StatsBase, Random, Distributions
+# This file can be run with the command "julia --threads 10 armtest.jl"
+# It runs a parameter grid search of the Attraction-Repulsion model presented
+# in Axelrod et al. (https://www.pnas.org/doi/abs/10.1073/pnas.2102139118).
+# A few modifications were made for this model to reflect affective polarization
+# instead of ideological polarization. The development and exploration of this
+# code can be reviewed here: https://github.com/matthewbbone/ARM-2D/tree/main/julia
+# I've separately added the working paper that is reporting on the dynamics of this
+# model relative Axelrod et al.'s original model.
+
+using PlotlyJS, Statistics, BenchmarkTools, DataFrames, JSON, StatsBase, Random, Distributions, ProgressBars
 
 mutable struct Agent
     id::Int16
@@ -27,9 +36,9 @@ function limit(val::Float64, method::String)
         
 end
 
-function interact(agent1::Agent, agent2::Agent, dist::Float64, affect::Bool)
+function interact(agent1::Agent, agent2::Agent, dist::Float64, affect_ad::Bool)
     
-    if !affect
+    if !affect_ad
         if abs(dist) <= agent1.T
             agent1.I = limit(agent1.I + agent1.R * dist, "trunc")
         else
@@ -46,7 +55,7 @@ function interact(agent1::Agent, agent2::Agent, dist::Float64, affect::Bool)
 
 end
 
-function step(agents::Vector{Agent}, affect::Bool)
+function step(agents::Vector{Agent}, affect_ad::Bool, affect_h::Bool)
     
     agent1::Agent = rand(agents)
     agent2::Agent = Agent(0,0,0,0,0,'A')
@@ -58,21 +67,25 @@ function step(agents::Vector{Agent}, affect::Bool)
     end
     
     dist::Float64 = agent2.I - agent1.I
-    prob::Float64 = .5^(abs(dist) / agent1.E)
+    if !affect_h
+        prob = .5^(abs(dist) / agent1.E)
+    else
+        prob = agent1.group == agent2.group ? 1 - agent1.E : agent1.E
+    end
 
     if rand(Float64) <= prob && dist != 0
-        interact(agent1, agent2, dist, affect)
+        interact(agent1, agent2, dist, affect_ad)
     end
 
 end
 
-function runARM(cycles::Int64, agents::Vector{Agent}, affect::Bool)
+function runARM(cycles::Int64, agents::Vector{Agent}, affect_ad::Bool, affect_h::Bool)
 
     positions::Vector{Vector{Float64}} = []
 
     for s in 1:cycles
         for i in 1:length(agents)
-            step(agents, affect::Bool)
+            step(agents, affect_ad::Bool, affect_h::Bool)
         end
         push!(positions, [agent.I for agent in agents])
     end
@@ -81,13 +94,14 @@ function runARM(cycles::Int64, agents::Vector{Agent}, affect::Bool)
 
 end
 
-function constantSuite(trials::Int64,cycles::Int64, n_agents::Int64, E::Vector{Float64}, T::Vector{Float64}, R::Vector{Float64}, affect::Bool, summary::Bool)
+function constantSuite(trials::Int64,cycles::Int64, n_agents::Int64, E::Vector{Float64}, T::Vector{Float64}, R::Vector{Float64}, affect_ad::Bool, affect_h::Bool, summary::Bool)
 
     results::Vector{Any} = zeros(length(E) * length(T) * length(R))
     lk = ReentrantLock()
     
-    Threads.@threads for e in eachindex(E)
-        for t in eachindex(T)
+    
+    for e in ProgressBar(eachindex(E))
+        Threads.@threads for t in eachindex(T)
             for r in eachindex(R)
                 res::Vector{Vector{Vector{Float64}}} = []
                 for tr=1:trials
@@ -98,7 +112,7 @@ function constantSuite(trials::Int64,cycles::Int64, n_agents::Int64, E::Vector{F
                         push!(agents, Agent(i, limit(randn() / 5 + mean, "norm"), E[e], T[t], R[r], group))
                     end
                     #agents = [Agent(i, limit(randn() / 5 + .5, "norm"), E[e], T[t], R[r], rand(['A','B'])) for i=1:n_agents]
-                    push!(res, runARM(cycles, agents, affect))
+                    push!(res, runARM(cycles, agents, affect_ad, affect_h))
                 end
                 if summary
                     summ = avgSeries(res)
@@ -166,7 +180,7 @@ function agentDist(agents::Vector{Agent})
 
 end
 
-function compareHists(agents1::Vector{Agent}, label1::String, agents2::Vector{Agent}, label2::String, cycles::Vector{Int64}, affect::Bool) 
+function compareHists(agents1::Vector{Agent}, label1::String, agents2::Vector{Agent}, label2::String, cycles::Vector{Int64}, affect_ad::Bool, affect_h::Bool) 
 
     p = make_subplots(
       rows=length(cycles), 
@@ -181,8 +195,8 @@ function compareHists(agents1::Vector{Agent}, label1::String, agents2::Vector{Ag
   
       if i > 1
   
-        runARM(cycles[i] - cycles[i-1], agents1, affect)
-        runARM(cycles[i] - cycles[i-1], agents2, affect)
+        runARM(cycles[i] - cycles[i-1], agents1, true, false)
+        runARM(cycles[i] - cycles[i-1], agents2, true, true)
   
       end
       
@@ -200,7 +214,7 @@ function compareHists(agents1::Vector{Agent}, label1::String, agents2::Vector{Ag
   
     relayout!(
       p, 
-      title_text="Comparing $(label1) to $(label2)", 
+      #title_text="Comparing $(label1) to $(label2)", 
       showlegend=false, 
       width=500, 
       height=200*length(cycles),
@@ -234,10 +248,12 @@ function compareHists(agents1::Vector{Agent}, label1::String, agents2::Vector{Ag
         heat_dat = vcat(heat_dat, reshape(row, 1, length(row)))
     end
 
-    return plot(
-        heatmap(z=heat_dat),
-        Layout(height = 500, width=550)
-    )
+    # return plot(
+    #     heatmap(z=heat_dat),
+    #     Layout(height = 500, width=550)
+    # )
+
+    return heatmap(z=heat_dat, x=range1, y=range2, zmin=0, zmax=.25)
 end
 
 println("number threads: ", Threads.nthreads())
@@ -246,15 +262,14 @@ const E = [i for i in .05:.05:.95]
 const T = [i for i in .05:.05:.95]
 const R = [i for i in .05:.05:.95]
 
-@time results = constantSuite(10, 25000, 100, E, T, R, false, true);
+@time results = constantSuite(10, 25000, 100, E, T, R, false, true, true);
 
 #avg_results = getAvgResults(results);
 
 js = JSON.json(results)
 
-open("id_results.json", "w") do f
+open("h_affect_results.json", "w") do f
     write(f, js)
 end
 
-# julia --threads 20 armtest.jl 
-# 225.102388 seconds
+# julia --threads 10 armtest.jl 
